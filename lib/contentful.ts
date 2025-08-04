c2cc4ab47ed7e812228f862d96f26a099347a5ea
kingsley achumie <kingsonly13c@gmail.com>
2025-08-04 15:18:00 +0100

    wip

--- lib/contentful.ts
+++ lib/contentful.ts
@@ -1,5 +1,9 @@
 import { createClient } from 'contentful';
 
+if (!process.env.CONTENTFUL_SPACE_ID || !process.env.CONTENTFUL_ACCESS_TOKEN) {
+    throw new Error("❌ Missing Contentful environment variables: CONTENTFUL_SPACE_ID or CONTENTFUL_ACCESS_TOKEN");
+}
+
 const contentfulClient = createClient({
     space: process.env.CONTENTFUL_SPACE_ID!,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN!,
